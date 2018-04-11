397fb61ed6116835ceaae4f80ec9708bda72cb4e
David Caron <dcaron05@gmail.com>
2018-04-11 15:33:54 -0400

    fix std::numeric_limits<unsignedshort>::max() default value

--- generators/utils.py
+++ generators/utils.py
@@ -94,14 +94,17 @@ def parameter_default_value(param):
     if val:
         # if val in class_enums_names:
         #     val = "Class::" + val
-        val = "=" + val.replace(" ", "")  # fix for exponent and float values parsed with added spaces
+
+        # fix for exponent and float values parsed with added spaces
+        val = "=" + val.replace(" ", "")
+        # fix for std::numeric_limits<unsignedshort>::max()
+        val = val.replace("unsignedshort", "unsigned short")
     return val
 
 
 def make_pybind_argument_list(cpp_parameters):
     if len(cpp_parameters) == 0:
         return ""
-    # types = ", ".join([init_param_type(p) for p in self.params])
 
     names = ", ".join(['"%s"_a%s' % (p["name"], parameter_default_value(p)) for p in cpp_parameters])
     return ", " + names
