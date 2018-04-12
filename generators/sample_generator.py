20e932081c3435649be2ce0e7e3dea220a96b5fc
David Caron <dcaron05@gmail.com>
2018-04-12 14:54:08 -0400

    fix namespace in CUSTOM_OVERLOAD_TYPES

--- generators/sample_generator.py
+++ generators/sample_generator.py
@@ -1,10 +1,16 @@
-from generators.main_generator import generate, write_stuff_if_needed
+from generators.main_generator import generate, write_stuff_if_needed, get_headers
 
 
 def main():
+    # modules = ["filters"]
+    # headers = get_headers(modules)
     headers = [
-        ("stereo", "stereo_matching.h", ""),
-        ("io", "file_io.h", ""),
+        ("", "pcl_base.h", ""),
+        ("filters", "filter.h", ""),
+        ("filters", "fast_bilateral.h", ""),
+        ("filters", "median_filter.h", ""),
+        ("filters", "convolution.h", ""),
+        ("filters", "fast_bilateral_omp.h", ""),
     ]
     generated_headers = generate(headers)
     write_stuff_if_needed(generated_headers, delete_others=False)
