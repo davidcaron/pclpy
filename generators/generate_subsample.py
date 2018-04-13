ef6319fd4fae2e4152ae4b17e1a980c44f4c203a
David Caron <dcaron05@gmail.com>
2018-04-12 23:20:13 -0400

    file rename

---
+++ generators/generate_subsample.py
@@ -0,0 +1,16 @@
+from generators.generate_pybind11_bindings import generate, write_stuff_if_needed, get_headers
+
+
+def main():
+    # modules = ["filters"]
+    # headers = get_headers(modules)
+    headers = [
+        ("", "pcl_base.h", ""),
+        ("features", "feature.h", ""),
+    ]
+    generated_headers = generate(headers)
+    write_stuff_if_needed(generated_headers, delete_others=False)
+
+
+if __name__ == '__main__':
+    main()
