5bbd60de1da1f03d1d8afad96da91274dfa5ec45
David Caron <dcaron05@gmail.com>
2018-04-13 16:38:58 -0400

    unfinished test

--- pclpy/tests/test.py
+++ pclpy/tests/test.py
@@ -2,6 +2,7 @@ import pytest
 import os
 import numpy as np
 import laspy
+import types
 
 from pclpy import pcl
 import pclpy
@@ -24,6 +25,18 @@ def test_hi():
 
 def test_size_xyz(xyz):
     assert xyz.size() == 10
+
+
+def test_initialize_everything():
+    modules = [m for m in dir(pcl) if not m.startswith("__") and m[0].islower()]
+
+    for module_name in modules:
+        module = getattr(pcl, module_name)
+        sub_modules = [c for c in dir(module) if not c.startswith("__")]
+        for name in sub_modules:
+            class_ = getattr(module, name)
+            if isinstance(class_, types.ModuleType):
+                print(class_, type(class_))
 
 
 # def test_points(xyz):
