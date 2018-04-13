6bc2478b28ea73f8ae75cb7376d8d011a1bb62ff
David Caron <dcaron05@gmail.com>
2018-04-13 08:45:23 -0400

    split function flag_overload_and_templated

--- generators/definitions/method.py
+++ generators/definitions/method.py
@@ -190,7 +190,7 @@ def flag_templated_methods(other_methods: List[Method]):
                 method.templated_types[type_name] = types
 
 
-def flag_overload_methods(other_methods: List[Method], needs_overloading: List[str] = None):
+def flag_overloaded_methods(other_methods: List[Method], needs_overloading: List[str] = None):
     templated_method_names = [m.cppmethod["name"] for m in other_methods if m.templated_types]
     # flag methods that need to be called with a lambda (same name and same parameters as a templated method)
     for method in other_methods:
@@ -222,7 +222,7 @@ def split_methods_by_type(methods: List[CppMethod],
     others = [Method(m) for m in chain(other_methods, setters, getters)]
 
     flag_templated_methods(others)
-    flag_overload_methods(others, needs_overloading)
+    flag_overloaded_methods(others, needs_overloading)
 
     variables = list(map(Variable, class_variables))
     constructors = list(map(Constructor, constructors_methods))
