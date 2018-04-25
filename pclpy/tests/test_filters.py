dd6cc872173da1dd568c59438a3ee569b8da6431
David Caron <dcaron05@gmail.com>
2018-04-25 14:57:20 -0400

    add test: test_extract_indices_simple

---
+++ pclpy/tests/test_filters.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+import numpy as np
+import laspy
+
+from pclpy import pcl
+import pclpy
+
+
+def test_data(*args):
+    return os.path.join("test_data", *args)
+
+
+def test_extract_indices_simple():
+    pc = pclpy.io.las.read_las(test_data("street_thinned.las"), colors=False)
+    extract = pcl.filters.ExtractIndices.PointXYZ()
+    extract.setInputCloud(pc)
+    pi = pcl.PointIndices()
+    pi.indices.append(3)
+    extract.setIndices(pi)
+    out = pcl.PointCloud.PointXYZ()
+    extract.filter(out)
+    assert np.allclose(out.xyz, np.array([[12.259, -0.124, 21.9086]], "f"))
