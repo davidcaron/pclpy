b5d8e18bbcd63d917893b04ec338eb2305df2b59
David Caron <dcaron05@gmail.com>
2018-05-09 15:56:08 -0400

    enlever stl et point_cloud.h

--- generators/config.py
+++ generators/config.py
@@ -14,8 +14,6 @@ common_includes = """
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/eigen.h>
-#include <pybind11/stl.h>
-#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
 namespace py = pybind11;
--- pclpy/src/generated_modules/visualization/window.cpp
+++ pclpy/src/generated_modules/visualization/window.cpp
@@ -2,8 +2,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/eigen.h>
-#include <pybind11/stl.h>
-#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
 namespace py = pybind11;
