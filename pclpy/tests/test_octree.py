39db261a467d2b74fad18499850fd7ebd14f09d9
David Caron <dcaron05@gmail.com>
2018-05-23 12:07:55 -0400

    add octree voxel centroid test

---
+++ pclpy/tests/test_octree.py
@@ -0,0 +1,21 @@
+import pytest
+import os
+import numpy as np
+
+from pclpy import pcl
+import pclpy
+
+
+def test_data(*args):
+    return os.path.join("test_data", *args)
+
+
+def test_voxel_centroid_simple():
+    pc = pclpy.io.las.read(test_data("street_thinned.las"), "PointXYZ")
+    vox = pcl.octree.OctreePointCloudVoxelCentroid.PointXYZ(0.3)
+    vox.setInputCloud(pc)
+    vox.addPointsFromInputCloud()
+    centroids = pcl.vectors.PointXYZ()
+    vox.getVoxelCentroids(centroids)
+    assert len(centroids) == 3148
+
