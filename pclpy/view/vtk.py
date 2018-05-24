91615edc1bda51a55b79653130873f087d2554e4
David Caron <dcaron05@gmail.com>
2018-05-24 14:53:00 -0400

    PointXYZ displayed by 'z' field (default) or random color

--- pclpy/view/vtk.py
+++ pclpy/view/vtk.py
@@ -1,4 +1,3 @@
-
 from pclpy import pcl
 
 
@@ -10,12 +9,14 @@ class Viewer:
         self.clouds = clouds
         self.overlay = overlay
 
+        self.point_xyz_random_color = False
+
         self.viewer = pcl.visualization.PCLVisualizer("viewer")
 
         if overlay:
             self.viewer.setBackgroundColor(*self.BG_COLOR, 0)
             for n, pc in enumerate(clouds, 1):
-                handler = make_color_handler(pc)
+                handler = self.make_color_handler(pc, glasbey_lut_id=n)
                 name = "cloud%s" % n
                 self.viewer.addPointCloud(pc, handler, name, viewport=0)
                 self.viewer.setPointCloudRenderingProperties(pcl.visualization.PCL_VISUALIZER_POINT_SIZE,
@@ -27,7 +28,7 @@ class Viewer:
             for n, pc in enumerate(clouds, 1):
                 self.viewer.createViewPort((n - 1) * vp_width, 0.0, n * vp_width, 1.0, n)
                 self.viewer.setBackgroundColor(*self.BG_COLOR, n)
-                handler = make_color_handler(pc)
+                handler = self.make_color_handler(pc, glasbey_lut_id=n)
                 name = "cloud%s" % n
                 self.viewer.addPointCloud(pc, handler, name, viewport=n)
                 self.viewer.setPointCloudRenderingProperties(pcl.visualization.PCL_VISUALIZER_POINT_SIZE,
@@ -42,13 +43,16 @@ class Viewer:
         while not self.viewer.wasStopped():
             self.viewer.spinOnce(50)
 
-
-def make_color_handler(pc):
-    if isinstance(pc, pcl.PointCloud.PointXYZRGBA):
-        return pcl.visualization.PointCloudColorHandlerRGBAField.PointXYZRGBA(pc)
-    elif isinstance(pc, pcl.PointCloud.PointXYZRGB):
-        return pcl.visualization.PointCloudColorHandlerRGBField.PointXYZRGB(pc)
-    elif isinstance(pc, pcl.PointCloud.PointXYZ):
-        return pcl.visualization.PointCloudColorHandler.PointXYZ()
-    elif isinstance(pc, pcl.PointCloud.PointXYZI):
-        return pcl.visualization.PointCloudColorHandlerGenericField.PointXYZI(pc, "intensity")
+    def make_color_handler(self, pc, glasbey_lut_id=0):
+        if isinstance(pc, pcl.PointCloud.PointXYZRGBA):
+            return pcl.visualization.PointCloudColorHandlerRGBAField.PointXYZRGBA(pc)
+        elif isinstance(pc, pcl.PointCloud.PointXYZRGB):
+            return pcl.visualization.PointCloudColorHandlerRGBField.PointXYZRGB(pc)
+        elif isinstance(pc, pcl.PointCloud.PointXYZ):
+            if self.point_xyz_random_color:
+                color = pcl.common.GlasbeyLUT.at(glasbey_lut_id)
+                return pcl.visualization.PointCloudColorHandlerCustom.PointXYZ(pc, color.r, color.g, color.b)
+            else:
+                return pcl.visualization.PointCloudColorHandlerGenericField.PointXYZ(pc, "z")
+        elif isinstance(pc, pcl.PointCloud.PointXYZI):
+            return pcl.visualization.PointCloudColorHandlerGenericField.PointXYZI(pc, "intensity")
