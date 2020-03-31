52167532e9cfb05261ffbd9752149b618eeeca3a
David Caron <dcaron05@gmail.com>
2020-03-31 17:32:07 -0400

    fix conda environment for ci build

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,7 @@
 
 version="1.9.1"
 
-$PYTHON -m pip install https://github.com/davidcaron/CppHeaderParser/archive/master.zip
+python -m pip install https://github.com/davidcaron/CppHeaderParser/archive/master.zip
 
 if [[ ! -d "pcl-pcl-$version" ]]; then
     scripts/download_pcl.sh
@@ -12,6 +12,6 @@ if [[ ! -d "pclpy/src/generated_modules" ]]; then
     scripts/generate_points_and_bindings.sh
 fi
 
-$PYTHON setup.py build
+python setup.py build
 
-$PYTHON -m pip install --no-deps .
+python -m pip install --no-deps .
